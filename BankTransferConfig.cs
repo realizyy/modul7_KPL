e8b436129eac3bcbc64d9aab3292dc72905ab6f4
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-09 12:48:28 +0800

    aplikasi bank transfer dengan runtime configuration.

---
+++ BankTransferConfig.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+
+namespace modul7_1302204035;
+
+public class BankTransferConfig
+{
+    private String CONFIG1;
+    private int CONFIG2;
+    private int CONFIG3;
+    private int CONFIG4;
+    private List<String> CONFIG5 = new()
+    {
+        "RTO (real-time)","SKN","RTGS","BI FAST"
+    };
+    private String CONFIG6;
+    private String CONFIG7;
+
+    public BankTransferConfig()
+    {
+        //get path
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        //get file
+        string file = File.ReadAllText(path + "\\bank_transfer_config.json");
+        //convert json
+        dynamic data = JsonConvert.DeserializeObject(file);
+        //set config value
+        CONFIG1 = data.lang;
+        CONFIG2 = data.transfer.threshold;
+        CONFIG3 = data.transfer.low_fee;
+        CONFIG4 = data.transfer.high_fee;
+        //CONFIG5 = data.method;
+        CONFIG6 = data.confirmation.en;
+        CONFIG7 = data.confirmation.id;
+    }
+    
+    //getter
+    public String getCONFIG1()
+    {
+        return CONFIG1;
+    }
+    
+    public int getCONFIG2()
+    {
+        return CONFIG2;
+    }
+    
+    public int getCONFIG3()
+    {
+        return CONFIG3;
+    }
+    
+    public int getCONFIG4()
+    {
+        return CONFIG4;
+    }
+    
+    public void printCONFIG5()
+    {
+        foreach (string item in CONFIG5)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    
+    public String selectMethods(int index)
+    {
+        return CONFIG5[index];
+    }
+
+    public String getCONFIG6()
+    {
+        return CONFIG6;
+    }
+    
+    public String getCONFIG7()
+    {
+        return CONFIG7;
+    }
+    
+    public void setLanguage(String lang)
+    {
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        string file = File.ReadAllText(path + "\\bank_transfer_config.json");
+        dynamic data = JsonConvert.DeserializeObject(file);
+        data.lang = lang;
+        String output = JsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(path + "\\bank_transfer_config.json", output);
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,137 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using modul7_1302204035;
+
 Console.WriteLine("Hello, World!");
+BankTransferConfig cfg = new BankTransferConfig();
+//bahasa en/id
+if (cfg.getCONFIG1() == "en")
+{
+    Console.WriteLine("Please insert the amount of money to transfer: ");
+}
+else
+{
+    Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+}
+//input amount of money
+int uang = Convert.ToInt32(Console.ReadLine());
+
+//calculate fee
+if (uang <= cfg.getCONFIG2())
+{
+    int biaya_transfer = cfg.getCONFIG3();
+    int total = (uang + biaya_transfer);
+        if (cfg.getCONFIG1() == "en")
+        {
+            Console.WriteLine("Transfer fee = " + biaya_transfer + " and total amount = " + total);
+        }
+        else
+        {
+            Console.WriteLine("Biaya transfer = " + biaya_transfer + " dan total biaya = " + total);
+        }
+} else
+{
+    int biaya_transfer = cfg.getCONFIG4();
+    int total = (uang + biaya_transfer);
+    if (cfg.getCONFIG1() == "en")
+    {
+        Console.WriteLine("Transfer fee = " + biaya_transfer + " and total amount = " + total);
+    }
+    else
+    {
+        Console.WriteLine("Biaya transfer = " + biaya_transfer + " dan total biaya = " + total);
+    }
+}
+//method
+if (cfg.getCONFIG1() == "en")
+{
+    Console.WriteLine("Select transfer method 1 - 4 : ");
+    cfg.printCONFIG5();
+    int inputMethod = Convert.ToInt32(Console.ReadLine());
+    if (inputMethod == 1)
+    {
+        Console.WriteLine("Method : "+cfg.selectMethods(0));
+    }
+    else if (inputMethod == 2)
+    {
+        Console.WriteLine("Method : " + cfg.selectMethods(1));
+    }
+    else if (inputMethod == 3)
+    {
+        Console.WriteLine("Method : " + cfg.selectMethods(2));
+    }
+    else if (inputMethod == 4)
+    {
+        Console.WriteLine("Method : " + cfg.selectMethods(3));
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
+else
+{
+    Console.WriteLine("Pilih metode transfer 1 - 4 : ");
+    cfg.printCONFIG5();
+    int inputMethod = Convert.ToInt32(Console.ReadLine());
+    if (inputMethod == 1)
+    {
+        Console.WriteLine("Metode : "+cfg.selectMethods(0));
+    }
+    else if (inputMethod == 2)
+    {
+        Console.WriteLine("Metode : " + cfg.selectMethods(1));
+    }
+    else if (inputMethod == 3)
+    {
+        Console.WriteLine("Metode : " + cfg.selectMethods(2));
+    }
+    else if (inputMethod == 4)
+    {
+        Console.WriteLine("Metode : " + cfg.selectMethods(3));
+    }
+    else
+    {
+        Console.WriteLine("Input tidak valid");
+    }
+}
+//confrimasi transaksi
+if (cfg.getCONFIG1() == "en")
+{
+    Console.WriteLine("Please type " + cfg.getCONFIG6() + " to confirm transaction");
+    //konfrimasi
+    String confrimasi = Console.ReadLine();
+    if (confrimasi == cfg.getCONFIG6())
+    {
+        Console.WriteLine("The transfer is complete");
+    }
+    else
+    {
+        Console.WriteLine("Transfer is canceled");
+    }
+}
+else
+{
+    Console.WriteLine("Ketik " + cfg.getCONFIG7() + " untuk konfirmasi transaksi");
+    //konfirmasi
+    String confrimasi = Console.ReadLine();
+    if (confrimasi == cfg.getCONFIG7())
+    {
+        Console.WriteLine("Transaksi berhasil");
+    }
+    else
+    {
+        Console.WriteLine("Transaksi dibatalkan");
+    }
+}
+//select bahasa
+Console.WriteLine("Pilih Bahasa/Select Language : \n1. Bahasa Indonesia\n2. English");
+int lang = Convert.ToInt32(Console.ReadLine());
+if (lang == 1)
+{
+    cfg.setLanguage("id");
+}
+else
+{
+    cfg.setLanguage("en");
+}
